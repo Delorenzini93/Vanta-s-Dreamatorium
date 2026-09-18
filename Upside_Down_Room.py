23a5c410f2c323466ebc07761b7455e82a11c9ad
Delorenzini93 <nicolasdelorenzini@gmail.com>
2026-09-17 21:00:51 -0300

    finished upside down room with puzzle and battle with mini boss

--- Upside_Down_Room.py
+++ Upside_Down_Room.py
@@ -1,15 +1,31 @@
 from inventory import add_item, remove_item, has_item
 import Status
+import battle
 
 come_from_trophies_room = False
 is_sword_taken = False
-x_switch_up = False
-y_switch_down = False
-z_switch_down = False
+dragon_switch_up = False
+mermaid_switch_down = False
+sphinx_switch_down = False
+is_family_crest_puzzle = False
 
 ########################
+def ghost_jester_battle():
+        result = battle.battle(
+            enemy_name="Ghost Jester",
+            enemy_hp=250,
+            enemy_attack=30
+        )
+
+        if result:
+            print("The Jester went away....for now")
+            return
+        else:
+            print("GAME OVER")
+            return
+########################
 def upside_down_room1():
-    global x_switch_up
+    global dragon_switch_up
     print("There's definitely an eerie feeling in here....there's a present silence in this room")
 
     while True:
@@ -24,22 +40,22 @@ def upside_down_room1():
                 print("You move to the middle of the room")
                 upside_down_room2()
             elif answer == 2:
-                print("There's switch in middle position with 'X' sign on it")
+                print("There's switch with a dragon carved on it")
                 while True:
-                    print("1. Turn it up")
-                    print("2. Turn it down")
-                    print("3. Leave it in middle position")
+                    print("1. Put the dragon in waking position")
+                    print("2. Put the dragon in death position")
+                    print("3. Leave the dragon in sleeping position")
 
                     try:
                         answer = int(input(""))
                         if answer == 1:
-                            print("The 'X' switch is up")
-                            x_switch_up = True
+                            print("The Dragon on the switch is awake")
+                            dragon_switch_up = True
                         elif answer == 2:
-                            print("The 'X' switch is down")
-                            x_switch_up = False
+                            print("The Dragon on the switch is dead")
+                            dragon_switch_up = False
                         elif answer == 3:
-                            print("You leave the 'X' switch as it was")
+                            print("You leave the dragon on the switch sleeping for now")
                             return
                     except ValueError:
                         print("Choose a valid action")
@@ -50,7 +66,7 @@ def upside_down_room1():
             print("Choose a valid action")
 #######################################
 def upside_down_room2():
-    global y_switch_down
+    global mermaid_switch_down
     print("There are many what seems to be kid's drawings covering the walls in this part of the room")
 
     while True:
@@ -65,22 +81,22 @@ def upside_down_room2():
                 print("You move to the final section of the room")
                 upside_down_room3()
             elif answer == 2:
-                print("There's switch in middle position with 'Y' sign on it")
+                print("There's switch with a mermaid carved on it")
                 while True:
-                    print("1. Turn it up")
-                    print("2. Turn it down")
-                    print("3. Leave it in middle position")
+                    print("1. Put the mermaid in surface position")
+                    print("2. Put the mermaid in drowning position")
+                    print("3. Leave the mermaid in sleeping position")
 
                     try:
                         answer = int(input(""))
                         if answer == 1:
-                            print("The 'Y' switch is up")
-                            y_switch_down = False
+                            print("The Mermaid on the switch is waving at sailors")
+                            mermaid_switch_down = False
                         elif answer == 2:
-                            print("The 'Y' switch is down")
-                            y_switch_down = True
+                            print("The Mermaid on the switch is drowned at the lake's bottom")
+                            mermaid_switch_down = True
                         elif answer == 3:
-                            print("You leave the 'Y' switch as it was")
+                            print("You leave the mermaid on the switch sleeping for now")
                             return
                     except ValueError:
                         print("Choose a valid action")
@@ -91,11 +107,11 @@ def upside_down_room2():
             print("Choose a valid action")
 #######################################
 def upside_down_room3():
-    global x_switch_up, y_switch_down, z_switch_down
+    global dragon_switch_up, mermaid_switch_down, sphinx_switch_down, is_family_crest_puzzle
     print("There is a considerable amount of paintings depicting people with different illnesses...I dont like this room")
 
     while True:
-        print("1. Move forward")
+        print("1. Touch the Family Crest")
         print("2. Move either side")
         print("3. Move backwards")
 
@@ -103,28 +119,34 @@ def upside_down_room3():
             answer = int(input("Where do we go?"))
 
             if answer == 1:
-                if x_switch_up and y_switch_down and z_switch_down:
+                if dragon_switch_up and mermaid_switch_down and sphinx_switch_down:
                     print("The wall crumbles revealing a hidden section of the room where the furniture hangs from the ceiling")
+                    print("A shining pendant falls on the floor near you")
+                    add_item('Mythril Necklace')
+                    print('You obtain MYTHRIL NECKLACE!')
+                    is_family_crest_puzzle = True
                     upside_down_room4()
                 else:
-                    print("There's a wall....unless I am a ghost, I'll be unable to pass it through")
+                    print("A fainting scream wails through the walls piercing your ears")
+                    print("An entity appears!")
+                    ghost_jester_battle()
             elif answer == 2:
-                print("There's switch in middle position with 'Z' sign on it")
+                print("There's switch with a Sphinx carved on it")
                 while True:
-                    print("1. Turn it up")
-                    print("2. Turn it down")
-                    print("3. Leave it in middle position")
+                    print("1. Put the Sphinx in waking position")
+                    print("2. Put the Sphinx in defensive position")
+                    print("3. Leave the Sphinx in sleeping position")
 
                     try:
                         answer = int(input(""))
                         if answer == 1:
-                            print("The 'Z' switch is up")
-                            z_switch_down = False
+                            print("The Sphinx on the switch is smiling enigmatically")
+                            sphinx_switch_down = False
                         elif answer == 2:
-                            print("The 'Z' switch is down")
-                            z_switch_down = True
+                            print("The Sphinx on the switch is menacingly on her feet")
+                            sphinx_switch_down = True
                         elif answer == 3:
-                            print("You leave the 'Z' switch as it was")
+                            print("You leave the Sphinx on the switch sleeping for now")
                             return
                     except ValueError:
                         print("Choose a valid action")
@@ -135,7 +157,7 @@ def upside_down_room3():
             print("Choose a valid action")
 #######################################
 def upside_down_room4():
-    global is_sword_taken
+    global is_sword_taken, is_family_crest_puzzle
     print("There's definitely an eerie feeling in here....there's a present silence in this room")
 
     while True:
@@ -147,7 +169,11 @@ def upside_down_room4():
             answer = int(input("Evom ew od erehw?"))
 
             if answer == 1:
-                print("The wall is too high to climb it")
+                if is_family_crest_puzzle:
+                    print("You go to the room with the unpleasant paintings")
+                    upside_down_room3()
+                else:
+                    print("The wall is too high to climb it")
             elif answer == 2:
                 if not is_sword_taken:
                     print("You found a BROADSWORD!")
